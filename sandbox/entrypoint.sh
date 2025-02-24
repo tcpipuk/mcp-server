f77d5981e691ecaf398eec2aadf1c480d41762fc
Tom Foster <tom@tcpip.uk>
2025-02-24 12:39:23 +0000

    Update sandbox image

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-exec socat UNIX-LISTEN:"${SANDBOX_SOCKET}",fork,mode=600 EXEC:"/bin/bash"
+set -e
+
+# shellcheck disable=SC1091
+source /opt/venv/bin/activate
+
+# Start socat as sandbox user
+exec socat \
+  UNIX-LISTEN:"${SANDBOX_SOCKET}",fork,mode=600,user=sandbox,group=sandbox \
+  EXEC:"/bin/bash"
