6a5427eef1639057f57842428656f169e4bdabc2
Tom Foster <tom@tcpip.uk>
2025-02-24 12:47:15 +0000

    Allow inbound 8080

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -4,7 +4,12 @@ set -e
 # shellcheck disable=SC1091
 source /opt/venv/bin/activate
 
+echo "Starting sandbox service..."
+echo "Listening for connections on 0.0.0.0:8080"
+
 # Start socat as sandbox user
 exec socat \
-  UNIX-LISTEN:"${SANDBOX_SOCKET}",fork,mode=600,user=sandbox,group=sandbox \
+  TCP-LISTEN:8080,fork,reuseaddr,bind=0.0.0.0 \
   EXEC:"/bin/bash"
+
+echo "Stopping sandbox service..."
