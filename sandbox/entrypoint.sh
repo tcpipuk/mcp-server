65c068821133be576772696a42f1dfb0e062fd2b
Tom Foster <tom@tcpip.uk>
2025-02-24 12:53:29 +0000

    Use pseudo terminal

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -7,9 +7,9 @@ source /opt/venv/bin/activate
 echo "Starting sandbox service..."
 echo "Listening for connections on 0.0.0.0:8080"
 
-# Start socat as sandbox user
+# Start socat as sandbox user with pseudo-terminal
 exec socat \
   TCP-LISTEN:8080,fork,reuseaddr,bind=0.0.0.0 \
-  EXEC:"/bin/bash"
+  EXEC:"/bin/bash -i",pty,stderr,setsid,sigint,sane
 
 echo "Stopping sandbox service..."
